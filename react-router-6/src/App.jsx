61f9c1bb11bad0dce2a72bdd66a3c0a302f05a00
elliotghr <gunsnroses.2617@gmail.com>
2025-05-06 17:03:32 -0600

    HashRouter

--- react-router-6/src/App.jsx
+++ react-router-6/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Header } from "./components/Header";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Acerca } from "./pages/Acerca";
 import { Home } from "./pages/Home";
 import { Contacto } from "./pages/Contacto";
@@ -75,6 +75,45 @@ function App() {
           <Route path="*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
+      <hr />
+      <HashRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/acerca" element={<Acerca />} />
+          {/* Redirección */}
+          <Route path="/about" element={<Navigate to="/acerca" />} />
+          <Route path="/contacto" element={<Contacto />}></Route>
+          <Route
+            path="/productos"
+            element={<Productos productos={productos} />}
+          ></Route>
+          {/* El nombre que asignemos después de los : es el nombre que recibirá la Page como parametro */}
+          <Route
+            path="/productos/:id"
+            element={<ProductoDetalle productos={productos} />}
+          ></Route>
+          <Route path="/servicios" element={<Servicios />}>
+            <Route index element={<ServiciosHome />} />
+            <Route path="garantia" element={<ServiciosGarantia />} />
+            <Route
+              path="lista"
+              element={<ServiciosLista servicios={servicios} />}
+            />
+            <Route
+              path=":id"
+              element={
+                <>
+                  <ServiciosLista servicios={servicios} />
+                  <ServicioDetalle servicios={servicios} />
+                </>
+              }
+            />
+            <Route path="politicas" element={<ServiciosPoliticas />} />
+          </Route>
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
