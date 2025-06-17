27490edae3cc265748beb9b4d9d9392099a17dd1
elliotghr <gunsnroses.2617@gmail.com>
2025-06-17 11:29:41 -0600

    useMemo

--- react-memo/src/components/ContadorHijo.jsx
+++ react-memo/src/components/ContadorHijo.jsx
@@ -1,7 +1,24 @@
-import React, { memo } from "react";
+import React, { memo, useMemo } from "react";
 
 const ContadorHijo = ({ contador, sumar, restar }) => {
   console.log("Hijo del Contador renderizado");
+
+  // Simulación de un cálculo pesado sin useMemo
+  // let superNumero = 0;
+
+  // for (let i = 0; i < 10000000000; i++) {
+  //   superNumero += 1;
+  // }
+
+  // Uso de useMemo para evitar cálculos innecesarios en cada renderizado
+  const superNumero = useMemo(() => {
+    let result = 0;
+    for (let i = 0; i < 10000000000; i++) {
+      result += 1;
+    }
+    return result;
+  }, []); // No dependencias, se calcula una sola vez
+
   return (
     <div>
       <h2>Hijo del Contador</h2>
@@ -10,6 +27,7 @@ const ContadorHijo = ({ contador, sumar, restar }) => {
         <button onClick={sumar}>+</button>
         <button onClick={restar}>-</button>
       </nav>
+      <h3>Super número: {superNumero}</h3>
     </div>
   );
 };
