aa7e52b216882bccfc9f25424906e9c61aab728b
elliotghr <gunsnroses.2617@gmail.com>
2025-08-12 17:58:25 -0600

    Curso React: 85. Reducers Hook useReducer

--- react-reducers/src/components/Contador.jsx
+++ react-reducers/src/components/Contador.jsx
@@ -1,16 +1,30 @@
 import { useReducer } from "react";
 
+const initialState = { contador: 0 };
+const TYPES = {
+  INCREMENTAR: "INCREMENTAR",
+  INCREMENTAR_5: "INCREMENTAR_5",
+  DECREMENTAR: "DECREMENTAR",
+  DECREMENTAR_5: "DECREMENTAR_5",
+  RESET: "RESET",
+};
+
 function reducer(state, action) {
   switch (action.type) {
-    case "INCREMENTAR":
+    case TYPES.INCREMENTAR:
       return { contador: state.contador + 1 };
-    case "DECREMENTAR":
+    case TYPES.DECREMENTAR:
       return { contador: state.contador - 1 };
+    case TYPES.INCREMENTAR_5:
+      return { contador: state.contador + action.payload };
+    case TYPES.DECREMENTAR_5:
+      return { contador: state.contador - action.payload };
+    case TYPES.RESET:
+      return initialState;
     default:
       return state;
   }
 }
-const initialState = { contador: 0 };
 
 export const Contador = () => {
   //   const [contador, setContador] = useState(0);
@@ -19,15 +33,23 @@ export const Contador = () => {
   //   const Incrementar = () => setContador(contador + 1);
   //   const Decrementar = () => setContador(contador - 1);
   // Usando useReducer
-  const Incrementar = () => dispatch({ type: "INCREMENTAR" });
-  const Decrementar = () => dispatch({ type: "DECREMENTAR" });
+  const Incrementar = () => dispatch({ type: TYPES.INCREMENTAR });
+  const Decrementar = () => dispatch({ type: TYPES.DECREMENTAR });
+  const Reset = () => dispatch({ type: TYPES.RESET });
+  const Incrementar5 = () =>
+    dispatch({ type: TYPES.INCREMENTAR_5, payload: 5 });
+  const Decrementar5 = () =>
+    dispatch({ type: TYPES.DECREMENTAR_5, payload: 5 });
 
   return (
     <div>
       <h2>Contador</h2>
       <p>{state.contador}</p>
+      <button onClick={Incrementar5}>Incrementar 5</button>
       <button onClick={Incrementar}>Incrementar</button>
+      <button onClick={Reset}>Resetear</button>
       <button onClick={Decrementar}>Decrementar</button>
+      <button onClick={Decrementar5}>Decrementar 5</button>
     </div>
   );
 };
